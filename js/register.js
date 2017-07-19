fb7aa3b09e5d35049487d96cd7cf6939386ab541
yt_guo <478922579@qq.com>
2017-07-19 21:22:45 +0800

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,12 +1,7 @@
-/*
-*	@time:2017/7/19
-*	@author:Bianca
-*	@version:1.0
-*/
 
 
 var pwd="notlow";//全局变量，判断密码是否是若密码，若为弱密码，禁止用户注册使用
-
+var pwd2="notlow";
 //判断用户口令强度
 $(function(){
 	$('#pass').keyup(function () {
@@ -53,6 +48,50 @@ $(function(){
 	});
 })
 
+$(function(){
+	$('#pass2').keyup(function () {
+		var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+		var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+		var enoughRegex = new RegExp("(?=.{6,}).*", "g");
+
+    //密码小于六位的时候，密码强度图片都为灰色，为弱密码
+		if (false == enoughRegex.test($(this).val())) {
+			$('#level2').removeClass('pw-weak');
+			$('#level2').removeClass('pw-medium');
+			$('#level2').removeClass('pw-strong');
+			$('#level2').addClass(' pw-defule');
+      pwd2="low";
+
+		}
+    //密码为八位及以上并且大小写字母数字特殊字符四项都包括,强度最强
+		else if (strongRegex.test($(this).val())) {
+			$('#level2').removeClass('pw-weak');
+			$('#level2').removeClass('pw-medium');
+			$('#level2').removeClass('pw-strong');
+			$('#level2').addClass(' pw-strong');
+      pwd2="notlow";
+
+		}
+    //密码为七位及以上且字母、数字、特殊字符三项中有两项，强度是中等
+		else if (mediumRegex.test($(this).val())) {
+			$('#level2').removeClass('pw-weak');
+			$('#level2').removeClass('pw-medium');
+			$('#level2').removeClass('pw-strong');
+			$('#level2').addClass(' pw-medium');
+      pwd2="notlow";
+		}
+    //如果密码为六位及以下，就算字母、数字、特殊字符三项都包括，也为弱密码
+		else {
+			$('#level2').removeClass('pw-weak');
+			$('#level2').removeClass('pw-medium');
+			$('#level2').removeClass('pw-strong');
+			$('#level2').addClass('pw-weak');
+      pwd2="low";
+
+		}
+		return true;
+	});
+})
 
 
 // 验证手机号注册表单数据有效性的函数
@@ -144,7 +183,7 @@ function Check2()// 验证表单数据有效性的函数
       return false;
     }
 
-    if(pwd=="low")
+    if(pwd2=="low")
     {
       alert('密码强度太弱，请重新输入！')
       return false;
